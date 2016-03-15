bbec69169683a8a2b1716449eeaf6233be71478e
LiTianpeng <litianpeng200@gmail.com>
2016-03-15 22:10:43 +0800

    Added tag and category migrations

---
+++ database/migrations/2016_03_15_140401_create_tag_migration.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tag_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tag');
+    }
+}
